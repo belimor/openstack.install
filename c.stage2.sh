0a906ff7126a9c14a4cc6aff8bb907c44a6bbe5d
belimor <dmitry.vagin@cybera.ca>
2014-06-18 19:45:56 +0000

    fix

--- c.stage2.sh
+++ c.stage2.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 controller="cloud"
-my_ip=$(hostname)
-NOVA_DBPASS=$(cat openstack_passwords.txt | grep dbnova_pass | awk '{print $3}')
-NOVA_PASS=$(cat openstack_passwords.txt | grep nova_pass | awk '{print $3}')
+my_ip="$(hostname)"
+NOVA_DBPASS="$(cat openstack_passwords.txt | grep dbnova | awk '{print $3}')"
+NOVA_PASS="$(cat openstack_passwords.txt | grep nova | awk '{print $3}')"
+RABBIT_PASS="$(cat openstack_passwords.txt | grep rabbit | awk '{print $3}')"
 
-apt-get install nova-compute-kvm python-guestfs
+apt-get install -y nova-compute-kvm python-guestfs
 dpkg-statoverride  --update --add root root 0644 /boot/vmlinuz-$(uname -r)
 
 echo "#!/bin/sh" > /etc/kernel/postinst.d/statoverride
@@ -19,14 +20,14 @@ crudini --set /etc/nova/nova.conf DEFAULT auth_strategy keystone
 crudini --set /etc/nova/nova.conf DEFAULT rpc_backend rabbit
 crudini --set /etc/nova/nova.conf DEFAULT rabbit_host ${controller}
 crudini --set /etc/nova/nova.conf DEFAULT rabbit_password ${RABBIT_PASS}
-crudini --set /etc/nova/nova.conf DEFAULT my_ip = ${my_ip}
-crudini --set /etc/nova/nova.conf DEFAULT vnc_enabled = True
-crudini --set /etc/nova/nova.conf DEFAULT vncserver_listen = 0.0.0.0
-crudini --set /etc/nova/nova.conf DEFAULT vncserver_proxyclient_address = ${my_ip}
-crudini --set /etc/nova/nova.conf DEFAULT novncproxy_base_url = http://${controller}:6080/vnc_auto.html
-crudini --set /etc/nova/nova.conf DEFAULT glance_host = ${controller}
+crudini --set /etc/nova/nova.conf DEFAULT my_ip ${my_ip}
+crudini --set /etc/nova/nova.conf DEFAULT vnc_enabled True
+crudini --set /etc/nova/nova.conf DEFAULT vncserver_listen 0.0.0.0
+crudini --set /etc/nova/nova.conf DEFAULT vncserver_proxyclient_address ${my_ip}
+crudini --set /etc/nova/nova.conf DEFAULT novncproxy_base_url http://${controller}:6080/vnc_auto.html
+crudini --set /etc/nova/nova.conf DEFAULT glance_host ${controller}
 
-crudini --set /etc/nova/nova.conf database connection mysql://nova:${NOVA_DBPASS}@${controlle}r/nova
+crudini --set /etc/nova/nova.conf database connection mysql://nova:${NOVA_DBPASS}@${controller}/nova
 
 crudini --set /etc/nova/nova.conf keystone_authtoken auth_uri http://${controller}:5000
 crudini --set /etc/nova/nova.conf keystone_authtoken auth_host ${controller}
