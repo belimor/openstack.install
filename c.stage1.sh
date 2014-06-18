8cb2705f5624d33f7f3f4433730d6ad2650b50bf
belimor <dmitry.vagin@cybera.ca>
2014-06-18 18:30:01 +0000

    codding

--- c.stage1.sh
+++ c.stage1.sh
@@ -4,6 +4,7 @@ apt-get update
 apt-get install -y ntp
 apt-get install -y python-mysqldb
 apt-get install -y python-software-properties
+apt-get install -y crudini
 apt-get update
 apt-get dist-upgrade -y
 reboot
