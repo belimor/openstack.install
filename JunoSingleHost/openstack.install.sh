a36d8aadbb772740720af58d47a0d89548af9366
Dmitry <dmitry.vaghin@cybera.ca>
2015-02-27 15:06:42 -0700

    juno installation for single host and puppet module

---
+++ JunoSingleHost/openstack.install.sh
@@ -0,0 +1,5 @@
+
+apt-get install -y ubuntu-cloud-keyring
+echo "deb http://ubuntu-cloud.archive.canonical.com/ubuntu" "trusty-updates/juno main" > /etc/apt/sources.list.d/cloudarchive-juno.list
+apt-get update && apt-get dist-upgrade -y
+reboot
