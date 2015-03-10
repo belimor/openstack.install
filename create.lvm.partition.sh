2b1e8cebaf20493a644c7127d98bc757c0ab9ca8
Dmitry <dmitry.vaghin@cybera.ca>
2015-03-09 18:55:06 -0600

    clean up the branch

--- create.lvm.partition.sh
+++ create.lvm.partition.sh
@@ -2,12 +2,11 @@
 
 source openstack.config
 
-CINDER_DEVICE="sdb"
 
 #echo -e "o\nn\np\n1\n\n\nw" | fdisk /dev/sdb
-echo -e "n\np\n1\n\n\nw" | fdisk /dev/sdb
+echo -e "n\np\n1\n\n\nw" | fdisk /dev/${CINDER_DEVICE}
 
 apt-get install -y lvm2
-pvcreate /dev/sdb1
-vgcreate cinder-volumes /dev/sdb1
-sed -i 's/[ "a/.*/" ]/[ "a/sdb/", "r/.*/"]/g' etc/lvm/lvm.conf
+pvcreate /dev/${CINDER_DEVICE}1
+vgcreate cinder-volumes /dev/${CINDER_DEVICE}1
+sed -i 's/[ "a/.*/" ]/[ "a/${CINDER_DEVICE}/", "r/.*/"]/g' etc/lvm/lvm.conf
--- create_partition.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source openstack.config
-
-CINDER_DEVICE="sdb"
-
-#echo -e "o\nn\np\n1\n\n\nw" | fdisk /dev/sdb
-echo -e "n\np\n1\n\n\nw" | fdisk /dev/sdb
