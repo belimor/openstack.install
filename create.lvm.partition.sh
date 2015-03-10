59ab68319e375ee375b4ef60dad4816398c2e955
Dmitry <dmitry.vaghin@cybera.ca>
2015-03-09 20:39:29 -0600

    small changes

--- create.lvm.partition.sh
+++ create.lvm.partition.sh
@@ -2,6 +2,8 @@
 
 source openstack.config
 
+echo "===============> Configuring LVM partition"
+sleep 5
 
 #echo -e "o\nn\np\n1\n\n\nw" | fdisk /dev/sdb
 echo -e "n\np\n1\n\n\nw" | fdisk /dev/${CINDER_DEVICE}
@@ -10,3 +12,6 @@ apt-get install -y lvm2
 pvcreate /dev/${CINDER_DEVICE}1
 vgcreate cinder-volumes /dev/${CINDER_DEVICE}1
 sed -i 's/[ "a/.*/" ]/[ "a/${CINDER_DEVICE}/", "r/.*/"]/g' etc/lvm/lvm.conf
+
+echo "===============> rebooting... (press ctr+c to stop)"
+sleep 10
