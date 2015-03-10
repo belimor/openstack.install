1c2447da5231dba44d6095f22731b4b7e886b3c8
Dmitry <dmitry.vaghin@cybera.ca>
2015-03-09 18:27:55 -0600

    new branch created

--- single_host/stage6.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-apt-get install -y apache2 memcached libapache2-mod-wsgi openstack-dashboard
-apt-get purge -y openstack-dashboard-ubuntu-theme
-
-service apache2 restart
-service memcached restart
-
